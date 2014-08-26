726f6f9ed974b6367085490f9f648ab50d1049c0
arowla <alison.rowland@gsa.gov>
2014-08-26 00:05:24 -0400

    Make a setUp method. Add a new test that uses show_closed

--- tests/test_fbopen.py
+++ tests/test_fbopen.py
@@ -15,17 +15,16 @@ from os import path
 
 
 class TestSynopsis(unittest.TestCase):
-    def test_synopsis(self):
+    def setUp(self):
         env_path = path.join(path.dirname(path.abspath(__file__)), '../.env')
         env = Dotenv(env_path)
         api_key = env.get('FBOPEN_API_KEY')
         
-        FBOpen = fbopen.FBOpen
-        FBOpen.init(api_key)    
-                    
-        #FBOpen.Opp.find(10)
-        
-        coll = FBOpen.Opp.search("software development", {'start' : 2})
+        self.client = fbopen.FBOpen
+        self.client.init(api_key, dev=True)
+
+    def test_synopsis(self):
+        coll = self.client.Opp.search("software development", {'start' : 2})
         
         print(coll.__dict__)
         
@@ -35,10 +34,16 @@ class TestSynopsis(unittest.TestCase):
         
         print(opp.id)
         
-       # finder = FBOpen.Opp.find(opp.id)
+       # finder = self.client.Opp.find(opp.id)
         
         
-        #FBOpen.Opp.search("bioinformatics", {'data_source' : 'grants.gov'})
+        #self.client.Opp.search("bioinformatics", {'data_source' : 'grants.gov'})
+
+    def test_satellite_misspelled(self):
+        coll = self.client.Opp.search("sattelite", {'show_closed': 'true'})
+        print(coll.__dict__)
+        self.assertEqual(coll.numFound, 3)
+
 
 
 if __name__ == '__main__':
